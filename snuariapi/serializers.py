96f0b6e57bd8f21b3d139165a881c88c213a356b
JW275 <2jwdream7029@naver.com>
2018-05-03 21:01:47 +0900

    add serializers for Club & add migrations to gitignore

--- snuariapi/serializers.py
+++ snuariapi/serializers.py
@@ -5,3 +5,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email',)
+
+class ClubListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Club
+        fields = ('id', 'name', 'scope', 'category', 'introduction',)
+
+class ClubDetailSerializer(serializers.HyperlinkedModelSerializer):
+    admin = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    members = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    waitings = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Club
+        fields = ('id', 'name', 'admin', 'members', 'waitings', 'scope', 'category', 'introduction',)
