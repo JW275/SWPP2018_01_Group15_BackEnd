89a8e98327147f5b081050b6ea5f5e02e0164a7f
JW275 <2jwdream7029@naver.com>
2018-05-03 20:16:54 +0900

    add model Club

--- snuariapi/models.py
+++ snuariapi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Club(models.Model):
+    name = models.CharField(max_length=20)
+    admin = models.ManyToManyField(User, related_name='club_admin')
+    members = models.ManyToManyField(User, related_name='club_members')
+    waitings = models.ManyToManyField(User, related_name='club_waitings')
+    scope = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    introduction = models.TextField()
+    
+
