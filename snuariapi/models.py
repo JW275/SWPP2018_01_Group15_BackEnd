2c464a3183b17f5461c4f784b8c57fd54b79c4ff
ilil96 <yh@DESKTOP-SC68LE7.localdomain>
2018-05-05 01:08:52 +0900

    Extend model User

--- snuariapi/models.py
+++ snuariapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
+now = datetime.datetime.now()
+
+class Student(User):
+    name = models.CharField(max_length=20)
+    college = models.CharField(max_length=20)
+    major = models.CharField(max_length=20)
+    admission_year = models.IntegerField(validators=[MaxValueValidator(now.year), MinValueValidator(1950)]) 
