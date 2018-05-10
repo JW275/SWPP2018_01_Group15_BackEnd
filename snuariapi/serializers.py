10baca98d0f112b8553e37aa33deb0a88876efac
ilil96 <37105138+ilil96@users.noreply.github.com>
2018-05-10 15:45:22 +0900

    add Profile and VerifyToken
    * Extend model User
    
    * modify serializers
    
    * modify model using AbstractUser
    
    * AUTH_USER_MODEL setting
    
    * add SignupView
    
    * change the way implement user
    
    * add default authentication
    
    * add VerifyToken for verifing snu-mail

--- snuariapi/serializers.py
+++ snuariapi/serializers.py
@@ -5,3 +5,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email',)
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'name', 'college', 'major', 'admission_year',)
+        extra_kwargs = {
+                'name': {'required': True, 'allow_blank': False},
+            'college': {'required': True, 'allow_blank': False},
+            'major': {'required': True, 'allow_blank': False},
+            'admission_year': {'required': True},
+        }
+
+class UserInfoSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.CharField(source='profile.name')
+    college = serializers.CharField(source='profile.college')
+    major = serializers.CharField(source='profile.major')
+    admission_year = serializers.IntegerField(source='profile.admission_year')
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'name', 'college', 'major', 'admission_year',)
+
